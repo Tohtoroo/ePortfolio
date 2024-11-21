bfc6707d7b52eed7cbd28eaec502a9c78f3ae6c9
Tohtoroo <e0725512@u.nus.edu>
2024-11-21 21:09:43 +0800

    Add openGraph image, description, and title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,28 @@ export const metadata: Metadata = {
   icons: {
     icon: './assets/Logo.png',
   },
-}
-
+  openGraph: {
+    title: 'Path of Reflection Main',
+    description: 'An e-Portfolio that reflects my journey through university.',
+    url: 'https://aeron-e-portfolio.vercel.app', // Your website URL
+    siteName: 'Path of Reflection',
+    images: [
+      {
+        url: '/assets/main-pg.png', // Path to your image
+        width: 1200, // Recommended width
+        height: 630, // Recommended height
+        alt: 'Path of Reflection Main Image', // Alt text for accessibility
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Path of Reflection Main',
+    description: 'An e-Portfolio that reflects my journey through university.',
+    images: ['/assets/main-pg.png'], // Path to your image
+  },
+};
 export default function Home() {
   return (
     <div>
